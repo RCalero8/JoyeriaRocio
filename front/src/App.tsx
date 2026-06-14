31efe36c05c071d98e23c262c71dac1e5d9b70eb
RCalero8 <calerorocio13@gmail.com>
2026-06-14 17:04:52 +0200

    Creamos el footer v1

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,8 +4,8 @@ import Navbar from "./component/Header/Navbar";
 import type { Section } from "./component/Header/Navbar";
 import Hero from "./component/hERO/Hero";
 /*import Catalogo from "./Catalogo";
-import Contacto from "./Contacto";
-import Footer from "./Footer";*/
+import Contacto from "./Contacto";*/
+import Footer from "./component/Footer/Footer";
 
 const App: React.FC = () => {
   const [section, setSection] = useState<Section>("inicio");
@@ -21,6 +21,7 @@ const App: React.FC = () => {
       <main className="main">
         {section === "inicio"   && <Hero onNav={onNav} />}
       </main>
+      <Footer/>
     </div>
   );
 };
