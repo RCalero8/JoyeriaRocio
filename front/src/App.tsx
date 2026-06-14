7b5a5ea2feb33be5c5f7e92b293b039e7aa73c7a
RCalero8 <calerorocio13@gmail.com>
2026-06-14 16:56:42 +0200

    Creamos el hero v1.1

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import "./App.css";
 import Navbar from "./component/Header/Navbar";
 import type { Section } from "./component/Header/Navbar";
-/*import Hero from "./Hero";
-import Catalogo from "./Catalogo";
+import Hero from "./component/hERO/Hero";
+/*import Catalogo from "./Catalogo";
 import Contacto from "./Contacto";
 import Footer from "./Footer";*/
 
@@ -12,11 +12,15 @@ const App: React.FC = () => {
 
   const onNav = (s: Section) => {
     setSection(s);
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
     <div className="app">
       <Navbar active={section} onNav={onNav} />
+      <main className="main">
+        {section === "inicio"   && <Hero onNav={onNav} />}
+      </main>
     </div>
   );
 };
